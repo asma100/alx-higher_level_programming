32092707c6e026bd9ee6ed4df907877363e266ce
asma <asmaa.alhadi100@gmail.com>
2023-10-31 18:48:47 -0700

    py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 output = ""
 for i in range(122, 96, -1):
-  if i % 2 == 0:
-    output += chr(i)
-  else:
-    output += chr(i-32)
+    if i % 2 == 0:
+        output += chr(i)
+    else:
+        output += chr(i-32)
+
 format_string = "{0}"
-print(format_string.format(output))
+print(format_string.format(output), end="")
