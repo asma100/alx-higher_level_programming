7ed0bdea7190bfca5ab3e357c44c45074b731e18
asma <asmaa.alhadi100@gmail.com>
2023-11-01 06:29:17 -0700

    2.pyman

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,1 @@
 #!/usr/bin/python3
-import random
-
-n = random.randint(-10, 10)
-
-if n > 0:
-    print(f"{n:d} is positive")
-elif n == 0:
-    print(f"{n:d} is zero")
-else:
-    print(f"{n:d} is negative")
