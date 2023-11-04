fbd718d02338cb51dfd2c49becae2cfdbc7b477f
asma <asmaa.alhadi100@gmail.com>
2023-11-04 12:12:37 -0700

    3.pymod

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add ,sub,mul,div
+
+    a = 10
+    b = 5
+
+    i = add(a, b)
+    print("{} + {} = {}".format(a, b, i))
+    i = sub(a, b)
+    print("{} - {} = {}".format(a, b, i))
+    i = mul(a, b)
+    print("{} * {} = {}".format(a, b, i))
+    i = div(a, b)
+    print("{} / {} = {}".format(a, b, i))
