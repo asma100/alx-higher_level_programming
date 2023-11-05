4530369a9cd4bf422ea675426475a4b82751aab8
asma <asmaa.alhadi100@gmail.com>
2023-11-05 04:54:19 -0800

    3.pymod

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-import sys
+    import sys
 
-aargs = sys.argv[1:]
-num_args = len(aargs)
+    aargs = sys.argv[1:]
+    num_args = len(aargs)
 
-print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
-for i, aarg in enumerate(aargs, start=1):
-    print(f"{i}: {aarg}")
+    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+    for i, aarg in enumerate(aargs, start=1):
+        print(f"{i}: {aarg}")
