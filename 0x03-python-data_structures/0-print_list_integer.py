26c225973d7a02f65e640cd35576462ec6566ac3
asma <asmaa.alhadi100@gmail.com>
2023-11-07 10:36:38 -0800

    py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    for i in my_list:
+        print(i)
+
+if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
-    for i in range(len(my_list)): 
-      print(my_list[i])
+    print_list_integer(my_list)
