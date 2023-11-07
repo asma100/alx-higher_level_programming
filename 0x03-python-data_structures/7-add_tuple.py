613001c5ff89658aa8632c86f0110db96911f8c9
asma <asmaa.alhadi100@gmail.com>
2023-11-07 14:34:39 -0800

    py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
     c1 = a[0] + b[0]
     c2 = a[1] + b[1]
-    
     return(c1, c2)
