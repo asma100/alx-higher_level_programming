e30ba49a3e10106ef8b3c6a3b011694043f96b72
asma <asmaa.alhadi100@gmail.com>
2023-11-20 18:22:41 -0800

    py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import sys
-
-def safe_print_integer_err(value):
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        print("Exception: Unknown format code 'd' for object of type '{}'".format(type(value).__name__), file=sys.stderr)
-        return False
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
