962b18c94afaf423ce1fa54975412f663986a935
asma <asmaa.alhadi100@gmail.com>
2023-11-22 15:59:16 -0800

    py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """Prints the last digit 
+
+    Args:
+        number (int): The number 
+
+    Returns:
+        int:  last digit.
+    """
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
