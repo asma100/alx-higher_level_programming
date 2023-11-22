c8e5cab2a4dbb6319c9847bba76f832a58b029d6
asma <asmaa.alhadi100@gmail.com>
2023-11-22 14:47:19 -0800

    py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+
+
+"""Square module."""
+
+
 class Square:
+
+
     """Defines a square."""
     def __init__(self, size=0):
         """Initializes a Square object with the specified size.
