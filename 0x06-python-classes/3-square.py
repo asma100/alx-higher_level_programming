f196c67afd9b04edd1c6b815ed7ab31164a7bdaf
asma <asmaa.alhadi100@gmail.com>
2023-11-22 14:09:04 -0800

    py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,6 +9,11 @@ class Square:
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
+
+
+        Raises:
+        TypeError: If size is not an integer 
+        ValueError: If size is less than 0
         """
 
         self.__size = size
