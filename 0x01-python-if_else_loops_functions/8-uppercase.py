9a9fda6131abde3d48905e5bf04e27126cce0f58
asma <asmaa.alhadi100@gmail.com>
2023-11-22 15:41:16 -0800

    py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+   
+    def uppercase(str):
+    for char in str:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
+    print()
