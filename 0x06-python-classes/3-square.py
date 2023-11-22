daeb487f19db32c80f996910df6a4fdbc19a802f
asma <asmaa.alhadi100@gmail.com>
2023-11-22 14:25:10 -0800

    py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-
-"""Square module"""
-
-
 class Square:
-"""Defines a square."""
+    """Defines a square"""
     def __init__(self, size=0):
-        """Initializes a Square object with the specified size.
-
-        Args:
-            size (int, optional): The size of the square. Defaults to 0.
-        """
-
+        """Initialises the data"""
         self.__size = size
-
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        """Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
+        """Returns current square area"""
+        return self.__size**2
