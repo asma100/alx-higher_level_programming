22934c5b28c18c212db85a8dbfd53e6cedb2e523
asma <asmaa.alhadi100@gmail.com>
2023-11-22 15:19:59 -0800

    py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,13 +6,15 @@
 
 class Square:
     """Defines a square."""
-    def __init__(self, size=0):
-        """Initializes a Square object with the specified size.
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a Square object with the specified size and position.
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
+            position (tuple, optional): The position of the square. Defaults to (0, 0).
         """
-        self.__size = size
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -40,6 +42,31 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Retrieves the position of the square.
+
+        Returns:
+            tuple: The position of the square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square.
+
+        Args:
+            value (tuple): The new position of the square.
+
+        Raises:
+            TypeError: If value is not a tuple of 2 positive integers.
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not isinstance(value[0], int) or not isinstance(value[1], int) or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """Calculates the area of the square.
 
@@ -53,5 +80,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                print("#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
