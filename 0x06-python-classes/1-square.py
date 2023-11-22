5f24cb82e357c0a9ed13244d3e9826bf8f984c45
asma <asmaa.alhadi100@gmail.com>
2023-11-22 12:52:48 -0800

    py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initializes a Square object with the specified size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
