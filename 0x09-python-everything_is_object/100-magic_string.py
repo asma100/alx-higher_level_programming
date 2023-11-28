68333dafaab4226a38a806cb40c5c324870d8add
asma <asmaa.alhadi100@gmail.com>
2023-11-28 13:59:02 -0800

    py

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,3 @@
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', -1) + 1
+    return "BestSchool" * magic_string.count + (", " + "BestSchool" * magic_string.count) * (magic_string.count - 1) if magic_string.count > 0 else ""
