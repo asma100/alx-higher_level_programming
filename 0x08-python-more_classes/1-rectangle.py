b4703054254d333bc4872f606a49c3f4c46b3472
asma <asmaa.alhadi100@gmail.com>
2023-12-11 13:54:59 -0800

    py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""class"""
+
+
 class Rectangle:
+    """width :
+      height :
+       """
     def __init__(self, width=0, height=0):
         self._width = width
         self._height = height
