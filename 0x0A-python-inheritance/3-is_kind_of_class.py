d7dda7782c0e0074a90516399eaa521f80d15f5d
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-05 17:43:49 -0800

    1

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module my list """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    obj :
+    a_class :
+    Returns : None
+    """
+    return isinstance(obj, a_class)
