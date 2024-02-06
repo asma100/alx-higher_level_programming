07d9c796af8e58d91bb17079279bec7b39e07ea4
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-05 19:08:18 -0800

    1

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module my list """
+
+
+class BaseGeometry:
+    """nothing"""
+    def area(self):
+        """nothing"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """nothing"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle (BaseGeometry):
+    """subclass"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
