a84b968afc11f9cffdb3592247236cd817e9fc26
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-06 16:45:56 -0800

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """read file"""
     with open(filename, "r", encoding='utf-8') as f:
-        a = f.read()
-        print(a)
+        for line in f:
+            print(line, end="")
