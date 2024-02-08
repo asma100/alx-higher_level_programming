1ae883fe3f080f51337e80dafee2fe57c82cd691
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-07 18:32:22 -0800

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-adding items
+Adds items to a JSON file, creating the file if it doesn't exist.
 """
+
 import sys
-"""module"""
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file  # Function to save a list to a JSON file
+from 6-load_from_json_file import load_from_json_file  # Function to load a list from a JSON file
 
 
-def add_items_to_file(items):
-"""
+def add_items_to_file(items: list[str]) -> None:
+    """
     Adds items to a JSON file, creating the file if it doesn't exist.
 
     Args:
@@ -18,6 +18,7 @@ def add_items_to_file(items):
     Raises:
         ValueError: If the loaded JSON data is invalid.
     """
+
     filename = "add_item.json"
     try:
         my_list = load_from_json_file(filename)
