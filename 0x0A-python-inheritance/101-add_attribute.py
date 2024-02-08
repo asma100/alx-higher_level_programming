c6625bee40ead2d7e57942990f31e8c9f43a7dab
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-07 17:10:55 -0800

    100

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,4 +5,6 @@
 
 def add_attribute(object, key, value):
     """ADD attribute"""
+    if not hasattr(object, "__dict__"):
+        raise TypeError("can't add new attribute")
     setattr(object, key, value)
