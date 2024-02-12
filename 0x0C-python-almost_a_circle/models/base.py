137da29610a4d6fcc3e0404d5d7f6191c8f76160
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-11 23:31:17 -0800

    0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None :
+            self.id = id
+        else:
+            __nb_objects += 1 
