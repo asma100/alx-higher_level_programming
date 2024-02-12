1a06579639d7ff29e949d6f51bd98e7e75ce18bd
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-12 15:56:01 -0800

    3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,96 +4,76 @@ from models.base import Base
 """import Base"""
 
 
-class Rectangle (Base):
-    """Rectangle class that inherits  from Base"""
-    
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.__set_width(width)
-        self.__set_height(height)
-        self.__set_x(x)
-        self.__set_y(y)
+class Rectangle:
+    """A rectangle with a width, height, x-coordinate, and y-coordinate."""
 
-    def __set_width(self, value):
-        """set w"""
-        if isinstance(value, int) and value > 0:
-            self.__width = value
-        else:
-            raise ValueError("Width must be a positive integer.")
+    def __init__(self, width, height, x=0, y=0):
+        """
+        Initializes a new Rectangle object.
 
-    def __get_width(self):
-        """get w"""
-        return self.__width
+        """
 
-    def __set_height(self, value):
-        """set h"""
-        if isinstance(value, int) and value > 0:
-            self.__height = value
-        else:
-            raise ValueError("Height must be a positive integer.")
+        self.int_validate("width", width, False)
+        self.int_validate("height", height, False)
+        self.int_validate("x", x, True)
+        self.int_validate("y", y, True)
 
-    def __get_height(self):
-        """get h"""
-        return self.__height
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-    def __set_x(self, value):
-        """set x"""
-        if isinstance(value, int):
-            self.__x = value
-        else:
-            raise ValueError("x must be an integer.")
+    def int_validate(self, name, value, non_negative):
+        """
+        Validates that the given value is a positive integer.
 
-    def __get_x(self):
-        """get x"""
-        return self.__x
+        """
 
-    def __set_y(self, value):
-        """set y"""
-        if isinstance(value, int):
-            self.__y = value
-        else:
-            raise ValueError("y must be an integer.")
-
-    def __get_y(self):
-        """get y"""
-        return self.__y
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if non_negative and value < 0:
+            raise ValueError(f"{name} must be >= 0")
 
     @property
     def width(self):
         """Getter for the width attribute."""
-        return self.__get_width()
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for the width """
-        self.__set_width(value)
+        """Setter for the width attribute."""
+        self.int_validate("width", value, True)
+        self.__width = value
 
     @property
     def height(self):
-        """Getter for the height """
-        return self.__get_height()
+        """Getter for the height attribute."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for the height """
-        self.__set_height(value)
+        """Setter for the height attribute."""
+        self.int_validate("height", value, True)
+        self.__width = value
 
     @property
     def x(self):
-        """Getter for the x """
-        return self.__get_x()
+        """Getter for the x-coordinate attribute."""
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for the x"""
-        self.__set_x(value)
+        """Setter for the x-coordinate attribute."""
+        self.int_validate("x", value, True)
+        self.__x = value
 
     @property
     def y(self):
-        """Getter for the y"""
-        return self.__get_y()
+        """Getter for the y-coordinate attribute."""
+        return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for the y"""
-        self.__set_y(value)
+        """Setter for the y-coordinate attribute."""
+        self.int_validate("y", value, True)
+        self.__y = value
