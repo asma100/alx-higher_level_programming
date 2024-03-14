9b9bde1aaba920bcab9f76a62189749cf272403f
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-03-13 20:05:13 -0700

    sql2

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,6 @@
 -- last
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_show_genres.genre_id
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_show_ratings.show_id
 ORDER BY rating DESC;
