06d2a34dbfc43511f8d1fd0c879db28ecad11bc2
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-03-13 19:34:37 -0700

    sql2

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,9 @@
 -- show by g
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+         LEFT JOIN `tv_show_genres` AS s
+	        ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+              ON s.`genre_id` = g.`id`
+	       ORDER BY t.`title`, g.`name`;
