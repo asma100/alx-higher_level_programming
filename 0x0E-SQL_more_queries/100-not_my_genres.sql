2e35cac8c69ada4120f6c3454f724e857a13a1bd
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-03-13 19:53:31 -0700

    sql2

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,11 @@
 --not my g
-SELECT name
-FROM tv_genres
-WHERE id NOT IN (
-SELECT genre_id
-FROM shows_genres
-WHERE show_id = (
-SELECT id
+SELECT tv_shows.title
 FROM tv_shows
-WHERE title = 'Dexter'
-)
-)
-ORDER BY name ASC;
+LEFT JOIN (
+    SELECT show_id
+        FROM shows_genres
+	    JOIN tv_genres ON shows_genres.genre_id = tv_genres.id
+	        WHERE tv_genres.name = 'Comedy'
+		) AS comedy_shows ON tv_shows.id = comedy_shows.show_id
+		WHERE comedy_shows.show_id IS NULL
+		ORDER BY tv_shows.title ASC;
