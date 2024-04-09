dbdf3ffd2fb1707387e6a4cf447e6cac5d3c7c52
asma100 <71214380+asma100@users.noreply.github.com>
2024-04-09 22:17:54 +0200

    4

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const BSquare = require('./5-square');
+
+class Square extends BSquare {
+  constructor(size) {
+      super(size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    }
+    else {
+      for (let i = 0; i < this.size; i++) {
+        let row = '';
+        for (let j = 0; j < this.size; j++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Square;
