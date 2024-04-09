9e13ac4fddb0fbb8c082d40bb3cadb022bd5663f
asma100 <71214380+asma100@users.noreply.github.com>
2024-04-09 04:15:43 +0200

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 if (process.argv.length === 3) {
   console.log('Argument found');
-} else if (process.argv.length < 2) {
+} else if (process.argv.length > 2) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
