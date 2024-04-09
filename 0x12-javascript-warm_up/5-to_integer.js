fe8989e9f5c76a080859595ac451277ee13e6a6c
asma100 <71214380+asma100@users.noreply.github.com>
2024-04-09 04:41:40 +0200

    task4

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-if (arg) {
-  console.log(arg1 + ' is ' + arg2);
-}
+const intNum = parseInt(arg1);
+
+if (Number.isNaN(argv)) {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + intNum);
+  }
