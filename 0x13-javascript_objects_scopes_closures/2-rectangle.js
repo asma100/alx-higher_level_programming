a9289aadb806e494e5552981f83d58e58ee28849
asma100 <71214380+asma100@users.noreply.github.com>
2024-04-09 13:59:12 +0200

    1

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0) { 
-            return this;
-          }
-          else {
-      this.w = w;
-      this.h = h;
-          }
-      
+    constructor (w, h) {
+      if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+        return this;
+      } else {
+        this.width = w;
+        this.height = h;
+      }
     }
   }
-  
   module.exports = Rectangle;
