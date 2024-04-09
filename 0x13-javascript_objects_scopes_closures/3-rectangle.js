adf133bcd429fec40ed2d3acd10dd2513b5c6629
asma100 <71214380+asma100@users.noreply.github.com>
2024-04-09 14:33:37 +0200

    3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,9 @@ class Rectangle {
     }
     print(){
       for (let i = 1; i <= this.height; i++) {
+          let row = '';
           for (let j = 1; i <= this.width; j++)
+            row += 'X';
             console.log('X'); 
           }
     }
