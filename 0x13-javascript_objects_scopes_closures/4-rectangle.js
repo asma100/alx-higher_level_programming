c6a0cd412d6096f75ceecf9bb9f732917c79cfef
asma100 <71214380+asma100@users.noreply.github.com>
2024-04-09 21:27:06 +0200

    4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-        return this;
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
     }
   
-    print () {
+  print () {
       for (let i = 0; i < this.height; i++) {
         let row = '';
         for (let j = 0; j < this.width; j++) {
-          row += 'X';
-        }
+            row += 'X';
+          }
         console.log(row);
       }
     }
-    rotate(){
+  rotate(){
       let x = this.height;
       this.height = this.width;
       this.width = x;
 
     }
-    double(){
+ double(){
       this.height = 2 * this.height;
       this.width = 2 * this.width;
     }
 }
-  
   module.exports = Rectangle;
