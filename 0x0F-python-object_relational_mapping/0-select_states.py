7cc53bb65ccf69f06ae74a01f11d752befc31d0b
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-04-13 06:52:06 -0700

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""the first task"""
-import MySQLdb
+import mysql.connector
 import sys
 
 def select_states(username, password, db_name):
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            password=password,
+            database=db_name
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+        cursor.close()
+        db.close()
+    except mysql.connector.Error as e:
+        print("MySQL Error {}: {}".format(e.errno, e.msg))
+        sys.exit(1)
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
     select_states(sys.argv[1], sys.argv[2], sys.argv[3])
