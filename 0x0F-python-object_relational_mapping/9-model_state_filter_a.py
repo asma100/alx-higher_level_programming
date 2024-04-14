19822c773bdfcfb982fa1aee6f05ea2fa815c95d
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-04-14 16:13:45 -0700

    9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,22 +6,23 @@ from model_state import Base, State
 import sys
 
 
-def print_states_with_a(username, password, db_name):
+def print_first_state(username, password, db_name):
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(username, password, db_name),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_with_a = session.query(State).order_by(State.id).all()
     if not states_with_a:
         print("Nothing")
     else:
         for state in states_with_a:
-            print("{}: {}".format(state.id, state.name))
+            if "a" in state.name:
+                print("{}: {}".format(state.id, state.name))
 
     session.close()
 
 
 if __name__ == "__main__":
-    print_states_with_a(sys.argv[1], sys.argv[2], sys.argv[3])
+    print_first_state(sys.argv[1], sys.argv[2], sys.argv[3])
