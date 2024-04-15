cb8c704acba7a94593c26b4a53cbe170b61eab6f
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-04-15 09:34:36 -0700

    15

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,6 +3,7 @@
 import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
