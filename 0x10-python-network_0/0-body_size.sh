95f0dfa32e1accc83acb2cad5634198ece5b2b9f
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-04-25 10:47:41 -0700

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,2 @@
 #!/bin/bash                                                                                                                                                                               #task0
-
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2x
