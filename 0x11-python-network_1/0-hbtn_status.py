59f0e6e0e8d6e982166562c4a258a51e1d97c248
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-04-26 10:59:41 -0700

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,4 +2,6 @@
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
    html = response.read()
-   print (html)
+print(f"Body response:\n\t- type: {type(html)}\n\t- content: {html}\n\t- utf8 content: {html.decode('utf-8')}")
+
+  
